26604bbaea00d832b34171013b1037b1427875f6
Jon Fei <jhfernan@mail.usf.edu>
2021-06-28 17:17:14 +0800

    Item routes updated for get all

--- routes/v1/items.js
+++ routes/v1/items.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
 
+const Item = require('../../models/item')
+
+const auth = require('../../middleware/auth')
+const util = require('../../middleware/utilities')
+
+const defFilter = { __v: 0 }
+const defSort = { sort: { brand: 1 }}
+
+// Required middleware on all routes
+// router.use('/items', auth.checkToken)
+
 // Items routes
-router.get('/items', function(req, res, next) {
+router.route('/items')
+.get(async function(req, res, next) {
+	const items = await Item.find({}, defFilter, defSort)
 	res.json(items)
 })
 
